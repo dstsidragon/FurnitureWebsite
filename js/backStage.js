26988e2f92a28b80d54e1f4b737bee633bf88f4b
PengSheng Lo <sim322on@yahoo.com.tw>
2021-04-20 16:12:46 +0800

    add

--- js/backStage.js
+++ js/backStage.js
@@ -1,19 +1,225 @@
-// C3.js
+
+
+function init2() {
+    c3js()
+}
+init2();
+
+function c3js(){
+    // C3.js
 let chart = c3.generate({
     bindto: '#chart', // HTML 元素綁定
     data: {
         type: "pie",
         columns: [
-        ['Louvre 雙人床架', 1],
-        ['Antony 雙人床架', 2],
-        ['Anty 雙人床架', 3],
-        ['其他', 4],
+            ['Louvre 雙人床架', 1],
+            ['Antony 雙人床架', 2],
+            ['Anty 雙人床架', 3],
+            ['其他', 4],
         ],
-        colors:{
-            "Louvre 雙人床架":"#DACBFF",
-            "Antony 雙人床架":"#9D7FEA",
+        colors: {
+            "Louvre 雙人床架": "#DACBFF",
+            "Antony 雙人床架": "#9D7FEA",
             "Anty 雙人床架": "#5434A7",
             "其他": "#301E5F",
         }
     },
 });
+}
+
+let sStr = "";
+
+showBackStageList()
+// function init(){
+// };
+
+//取得後臺資料 顯示在畫面
+const backStageTable = document.getElementById("backStageTable");
+function showBackStageList() {
+
+    sStr = ` <thead>
+      <tr>
+          <th>訂單編號</th>
+          <th>聯絡人</th>
+          <th>聯絡地址</th>
+          <th>電子郵件</th>
+          <th>訂單品項</th>
+          <th>訂單日期</th>
+          <th>訂單狀態</th>
+          <th>操作</th>
+      </tr>
+  </thead>`;
+    //取得伺服器訂單資料
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
+        {
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+            //   console.log(response.data.orders);
+            response.data.orders.forEach(function (item, i) {
+                // console.log(item);
+                let orderStatus = "";
+                if (item.paid == true) { orderStatus = "已處理" } else { orderStatus = "未處理" }
+                sStr += `<tr>
+    <td>${item.createdAt}</td>
+    <td>
+        <p>${item.user.name}</p>
+        <p>${item.user.tel}</p>
+    </td>
+    <td>${item.user.address}</td>
+    <td>${item.user.email}</td>
+    <td>
+        <p>${item.products[0].title}</p>
+    </td>
+    <td>${item.user.date}</td>
+    <td class="orderStatus">
+        <a href="#" id="orderStatus${i}" data-number="${i}" data-orderId="${item.id}">${orderStatus}</a>
+    </td>
+    <td>
+    <input type="button" id="delOrder${i}" class="delSingleOrder-Btn" data-orderId="${item.id}" value="刪除">
+    </td>
+</tr>`;
+
+            })
+            backStageTable.innerHTML = sStr;
+
+            //動態賦予事件
+            response.data.orders.forEach(function (item, i) {
+                //動態賦予刪除事件
+                document.getElementById(`delOrder${i}`).addEventListener("click", delSoneOneOrder, false);
+                document.getElementById(`orderStatus${i}`).addEventListener("click", mdiSoneOneOrder, false);
+
+            })
+
+
+        }).catch(function (error) {
+            // 失敗會回傳的內容
+            console.log(error);
+        })
+
+
+
+
+};
+
+
+//刪除特定訂單
+
+
+
+function delSoneOneOrder() {
+    // console.log(this.dataset);
+    // console.log(this.dataset.orderid);
+    const orderId = this.dataset.orderid;
+    axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders/${orderId}`, {
+
+        headers: {
+            'Authorization': token
+        }
+    }).then(function (response) {
+        console.log(response);
+        alert("刪除 1 筆訂單!")
+        showBackStageList();
+    }).catch(function (error) {
+        // 失敗會回傳的內容
+        console.log(error);
+    })
+}
+
+//刪除全部訂單
+const discardAllBtn = document.getElementById("discardAllBtn");
+// console.log(discardAllBtn)
+discardAllBtn.addEventListener("click", delAllOrder, false);
+function delAllOrder(e) {
+    e.preventDefault();
+    //取得伺服器訂單資料
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
+        {
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(function (response) {
+            // console.log(response);
+            //    console.log(response.data.orders.length);
+            if (response.data.orders.length == 0) { alert("沒有訂單資料!") } else {
+                axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders/`, {
+                    headers: {
+                        'Authorization': token
+                    }
+                }).then(function (response) {
+                    console.log(response);
+                    alert("刪除 全部訂單!")
+                    showBackStageList();
+                }).catch(function (error) {
+                    // 失敗會回傳的內容
+                    console.log(error);
+                })
+            }
+        }).catch(function (error) {
+            // 失敗會回傳的內容
+            console.log(error);
+        })
+
+
+
+
+};
+
+//修改訂單狀態
+function mdiSoneOneOrder(e){
+    e.preventDefault();
+    const orderId = this.dataset.orderid;
+    const orderNumber = this.dataset.number;
+// console.log(orderId)
+// console.log(orderNumber)
+    //取得伺服器訂單資料
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
+        {
+            headers: {
+                'Authorization': token
+            }
+        })
+        .then(function (response) {
+            
+        // console.log(response.data.orders);
+        let orderStatus = !response.data.orders[orderNumber].paid;
+        // console.log(orderStatus);
+
+
+
+        //修改伺服器資料
+        axios.put(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
+        {
+          "data": {
+            "id": orderId,
+            "paid":orderStatus
+          }
+        },
+        {
+          headers: {
+            'Authorization': token
+          }
+        }).then(function (response) {
+            alert("已改變 1筆訂單狀態!")
+            showBackStageList();
+            
+        }).catch(function (error) {
+            // 失敗會回傳的內容
+            console.log(error);
+        })
+
+
+
+}).catch(function (error) {
+    // 失敗會回傳的內容
+    console.log(error);
+})
+
+
+
+
+}
---
+++ js/fontSide.js
@@ -0,0 +1,262 @@
+
+let sStr = "";
+const myCartList = document.getElementById("myCartList");
+
+//初始化
+function init() {
+  showShppingCar();
+
+};
+init();
+
+
+
+//新增商品至購物車 
+const addCardBtn = document.getElementsByClassName("addCardBtn");
+// console.log(addCardBtn);
+// console.log( addCardBtn.length)
+
+for (let i = 0; i < addCardBtn.length; i++) {
+  // console.log(1);
+  addCardBtn[i].addEventListener('click', addCart, false)
+}
+function addCart(e) {
+  // console.log(1);
+  e.preventDefault();
+  // console.log(e.target.dataset.id);
+  axios.post(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts`,
+    {
+      "data": {
+        "productId": `${e.target.dataset.id}`,
+        "quantity": 1
+      }
+    })
+    .then(function (response) {
+      // 成功會回傳的內容
+      console.log(response.data)
+      alert("已加入購物車!")
+
+      showShppingCar();
+    }).catch(function (error) {
+      // 失敗會回傳的內容
+      console.log(error);
+    })
+
+};
+
+
+//取得購物車 顯示在畫面
+let cartItem = 0;
+function showShppingCar() {
+  axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts`)
+    .then(function (response) {
+      // 成功會回傳的內容
+      // console.log(response.data.carts);
+      //   console.log("成功");
+
+      sStr = ` <tr>
+  <th width="40%">品項</th>
+  <th width="15%">單價</th>
+  <th width="15%">數量</th>
+  <th width="15%">金額</th>
+  <th width="15%"></th>
+</tr>`;
+
+      // let aa =response.data.products.filter(function (item,i,arr){
+      //     return arr.indexOf(item) === i;
+      // });
+      // // console.log(aa);
+      let cartListPrice = 0;
+      cartItem = 0;
+      response.data.carts.forEach(function (item, i) {
+        // console.log(item);
+        sStr += `<tr >
+<td>
+    <div class="cardItem-title">
+        <img src="${item.product.images}" alt="">
+        <p>Antony ${item.product.title}</p>
+    </div>
+</td>
+<td>NT$${item.product.price}</td>
+<td>1</td>
+<td>NT$${item.product.price}</td>
+<td class="discardBtn" >
+    <a href="#" id="deleteSomeOne${i}" class="material-icons " data-id-clear='${item.id}'>
+        clear
+    </a>
+</td>
+</tr>`;
+        // console.log(item.id)
+        //加總金額
+        cartListPrice += item.product.price;
+        //紀錄筆數
+        cartItem += 1;
+      })
+      myCartList.innerHTML = sStr;
+
+
+      sStr = `
+ <tr>
+   <td>
+       <a href="#" id="discardAllBtn" class="discardAllBtn">刪除所有品項</a>
+   </td>
+   <td></td>
+   <td></td>
+   <td>
+       <p>總金額</p>
+   </td>
+   <td>NT$${cartListPrice}</td>
+ </tr>`;
+
+      //  console.log(sStr);
+      myCartList.innerHTML += sStr;
+
+
+      actDelListen();
+      //刪除購物車全部商品
+      let discardAllBtn = document.getElementById("discardAllBtn");
+      discardAllBtn.addEventListener('click', delAllCart, false);
+    })
+    .catch(function (error) {
+      // 失敗會回傳的內容
+      console.log(error);
+    })
+};
+
+
+
+
+//刪除購物車單件商品 
+
+//動態賦予監聽事件
+function actDelListen() {
+  for (let i = 0; i < cartItem; i++) {
+
+    document.getElementById(`deleteSomeOne${i}`).addEventListener('click', deloneCart, false)
+  }
+
+}
+function deloneCart(e) {
+  // console.log(1);
+  e.preventDefault();
+  // console.log(e.target.dataset.idClear);
+  axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts/${e.target.dataset.idClear}`)
+    .then(function (response) {
+      // 成功會回傳的內容
+      // console.log(response.data)
+      alert("成功刪除 1 筆商品!")
+      showShppingCar();
+
+    }).catch(function (error) {
+      // 失敗會回傳的內容
+      console.log(error);
+    })
+
+};
+
+
+
+
+//刪除購物車全部
+function delAllCart(e) {
+  e.preventDefault();
+  //確認伺服器購物車資料
+  axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts`)
+    .then(function (response) {
+      // console.log(response.data.carts.length)
+      if (response.data.carts.length == 0) { alert("購物車內無任何資料!!") } else {
+
+        //新增購物車訂單//刪除伺服器資料
+        axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts`)
+          .then(function (response) {
+            // 成功會回傳的內容
+            // console.log(response.data)
+            alert("成功刪除全部商品!")
+
+            showShppingCar();
+          }).catch(function (error) {
+            // 失敗會回傳的內容
+            console.log(error);
+          })
+
+
+      }
+    }).catch(function (error) {
+      // 失敗會回傳的內容
+      console.log(error);
+    })
+
+
+}
+
+
+//送出訂單
+const orderInfoBtn = document.querySelector(".orderInfo-btn");
+// console.log(orderInfoBtn);
+
+orderInfoBtn.addEventListener("click", addOrder, false);
+
+function addOrder(e) {
+  e.preventDefault();
+  const nameVal = document.getElementById("customerName").value;
+  const telVal = document.getElementById("customerPhone").value;
+  const emailVal = document.getElementById("customerEmail").value;
+  const addressVal = document.getElementById("customerAddress").value;
+  const tradessVal = document.getElementById("tradeWay").value;
+
+  // console.log(nameVal);
+  // console.log(telVal);
+  // console.log(emailVal);
+  // console.log(addressVal);
+  let Today=new Date();
+// console.log(Today)
+
+  if (nameVal == "" || telVal == "" || emailVal == "" || addressVal == "") {
+    alert("必填資料不得為空!!")
+  } else {
+    //確認伺服器購物車資料
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/carts`)
+      .then(function (response) {
+        // console.log(response.data.carts.length)
+        if (response.data.carts.length == 0) { alert("購物車內無任何資料!!") } else {
+
+          axios.post(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${pathApi}/orders
+        `,
+            {
+              "data": {
+                "user": {
+                  "name": `${nameVal}`,
+                  "tel": `${telVal}`,
+                  "email": `${emailVal}`,
+                  "address": `${addressVal}`,
+                  "payment": `${nameVal}`,
+                  // "date":`${ Today.getFullYear()}`+"/"+`${ (Today.getMonth()+1)}`+"/"+`${ Today.getDate() }`
+                  "date":`${ Today.getFullYear()}/${ (Today.getMonth()+1)}/${ Today.getDate() }`
+                }
+              }
+            })
+            .then(function (response) {
+              // 成功會回傳的內容
+              console.log(response.data)
+              alert("已送出訂單資料!")
+              orderForm.reset();
+              showShppingCar();
+            }).catch(function (error) {
+              // 失敗會回傳的內容
+              console.log(error);
+            })
+
+
+
+        }
+      }).catch(function (error) {
+        // 失敗會回傳的內容
+        console.log(error);
+      })
+
+
+  }
+
+
+
+}
