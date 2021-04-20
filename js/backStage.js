69716b4e83ab713432db796fe6ddf1bd7a76808d
PengSheng Lo <sim322on@yahoo.com.tw>
2021-04-20 18:18:42 +0800

    add c3JS

--- js/backStage.js
+++ js/backStage.js
@@ -1,31 +1,26 @@
 
 
-function init2() {
-    c3js()
-}
-init2();
-
-function c3js(){
-    // C3.js
-let chart = c3.generate({
-    bindto: '#chart', // HTML 元素綁定
-    data: {
-        type: "pie",
-        columns: [
-            ['Louvre 雙人床架', 1],
-            ['Antony 雙人床架', 2],
-            ['Anty 雙人床架', 3],
-            ['其他', 4],
-        ],
-        colors: {
-            "Louvre 雙人床架": "#DACBFF",
-            "Antony 雙人床架": "#9D7FEA",
-            "Anty 雙人床架": "#5434A7",
-            "其他": "#301E5F",
+function c3js(arr){
+    let chart = c3.generate({
+        bindto: '#chart', // HTML 元素綁定
+        data: {
+            columns: arr,
+            type: "donut",
+            colors: {
+                c3Top1: "#DACBFF",
+                c3Top2: "#9D7FEA",
+                c3Top3: "#5434A7",
+                c3Top4: "#301E5F"
+            }
+            
+        },
+        donut: {
+        title: "全品項營收比重"
         }
-    },
-});
+       
+    });
 }
+
 
 let sStr = "";
 
@@ -57,8 +52,83 @@ function showBackStageList() {
             }
         })
         .then(function (response) {
-            console.log(response);
-            //   console.log(response.data.orders);
+            // console.log(response);
+            // console.log(response.data.orders);
+            let productsInf = [];
+            let productsInfAry = [];
+            let productsInfObj = {};
+            response.data.orders.forEach(function (item, i) {
+                // console.log(item.products);
+
+                item.products.forEach(function (item2, i2) {
+                    productsInf = [];
+                    // console.log(item2.title)
+                    // console.log(item2.quantity)
+                    productsInf.push(item2.title);
+                    productsInf.push(item2.quantity);
+                    // console.log(productsInf)
+                    productsInfAry.push(productsInf);
+                })
+            })
+            // console.log(productsInfAry);
+            productsInfAry.forEach(function (product, i) {
+                // console.log(product)
+                // console.log(product[0]);
+
+                if (productsInfObj[product[0]] == undefined) {
+                    productsInfObj[product[0]] = product[1];
+                } else {
+                    productsInfObj[product[0]] += product[1];
+                }
+
+            })
+
+
+
+            // console.log(productsInfObj)
+
+            const itemKeyAry = Object.keys(productsInfObj);
+            // console.log(itemKeyAry)
+            let sortAry = [];
+            itemKeyAry.forEach(function (item, i) {
+                // console.log(item)
+                let sortObj = {};
+                sortObj[`${item}`] = productsInfObj[item]
+                sortAry.push(sortObj);
+                //    console.log(sortObj)
+            })
+            //改變排序
+            sortAry = sortAry.sort(function (a, b) {
+                return a[1] > b[1] ? 1 : -1;
+            });
+
+            // console.log(sortAry)
+            //前三的資料
+            let c3Ary = [];
+            for (let i = 0; i < 3; i++) {
+                let top3Ary = [];
+                // console.log(Object.keys(sortAry[i])[0]);
+                // console.log(Object.values(sortAry[i])[0]);
+                top3Ary.push(Object.keys(sortAry[i])[0]);
+                top3Ary.push(Object.values(sortAry[i])[0]);
+                c3Ary.push(top3Ary);
+            }
+            // console.log(c3Ary)
+            //PUSH 其他
+            let anotherAry = [];
+            let anotherTotal =0;
+            for (let i = 3; i < sortAry.length; i++) {
+                
+
+                // console.log(Object.values(sortAry[i])[0]);
+                anotherTotal+= Object.values(sortAry[i])[0]
+               
+            }
+            anotherAry.push("其他");
+            anotherAry.push(anotherTotal);
+            c3Ary.push(anotherAry);
+            // console.log(c3Ary[0][0])
+
             response.data.orders.forEach(function (item, i) {
                 // console.log(item);
                 let orderStatus = "";
@@ -94,6 +164,8 @@ function showBackStageList() {
 
             })
 
+            //刷新C3
+            c3js(c3Ary);
 
         }).catch(function (error) {
             // 失敗會回傳的內容
@@ -170,12 +242,12 @@ function delAllOrder(e) {
 };
 
 //修改訂單狀態
-function mdiSoneOneOrder(e){
+function mdiSoneOneOrder(e) {
     e.preventDefault();
     const orderId = this.dataset.orderid;
     const orderNumber = this.dataset.number;
-// console.log(orderId)
-// console.log(orderNumber)
+    // console.log(orderId)
+    // console.log(orderNumber)
     //取得伺服器訂單資料
     axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
         {
@@ -184,40 +256,40 @@ function mdiSoneOneOrder(e){
             }
         })
         .then(function (response) {
-            
-        // console.log(response.data.orders);
-        let orderStatus = !response.data.orders[orderNumber].paid;
-        // console.log(orderStatus);
+
+            // console.log(response.data.orders);
+            let orderStatus = !response.data.orders[orderNumber].paid;
+            // console.log(orderStatus);
 
 
 
-        //修改伺服器資料
-        axios.put(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
-        {
-          "data": {
-            "id": orderId,
-            "paid":orderStatus
-          }
-        },
-        {
-          headers: {
-            'Authorization': token
-          }
-        }).then(function (response) {
-            alert("已改變 1筆訂單狀態!")
-            showBackStageList();
-            
+            //修改伺服器資料
+            axios.put(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/${pathApi}/orders`,
+                {
+                    "data": {
+                        "id": orderId,
+                        "paid": orderStatus
+                    }
+                },
+                {
+                    headers: {
+                        'Authorization': token
+                    }
+                }).then(function (response) {
+                    alert("已改變 1筆訂單狀態!")
+                    showBackStageList();
+
+                }).catch(function (error) {
+                    // 失敗會回傳的內容
+                    console.log(error);
+                })
+
+
+
         }).catch(function (error) {
             // 失敗會回傳的內容
             console.log(error);
         })
-
-
-
-}).catch(function (error) {
-    // 失敗會回傳的內容
-    console.log(error);
-})
 
 
 
