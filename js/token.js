32257ee94d8328ea1fd8e29fdbdf5c95a5c948cb
dstsidragon <sim322on@yahoo.com.tw>
2021-09-24 17:52:51 +0800

    Update api url

--- js/token.js
+++ js/token.js
@@ -1,4 +1,5 @@
 let pathApi = "sim322on";
 let token = "4Vux1zvgshMUyCHwUDf149T08bB2";
 // let api_url_old = "https://hexschoollivejs.herokuapp.com";
-let api_url = "livejs-api.hexschool.io";
+// let api_url = "livejs-api.hexschool.io";
+let api_url = "https://livejs-api.hexschool.io/";
